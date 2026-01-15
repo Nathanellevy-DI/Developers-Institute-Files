0601a490892e03b4da37471625a70a9dc2a3d771
nathanellevy-di <nathanellevydi@gmail.com>
2026-01-15 13:56:23 +0200

    Add Week 3 Day 3 Exercise 1 and 2

---
+++ Week-3/Day-3/ExersiseXP/Exercise2/script.js
@@ -0,0 +1,13 @@
+function myMove() {
+    let elem = document.getElementById("animate");
+    let pos = 0;
+    let id = setInterval(frame, 1);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
